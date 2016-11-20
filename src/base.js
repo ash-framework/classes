dc2e325ccbe618367014b315ab3709b67573ef99
Richard Walker <digitalsadhu@gmail.com>
2016-11-20 15:38:52 +0100

    Move mixin code out of contstructor, fix name

--- src/base.js
+++ src/base.js
@@ -1,24 +1,29 @@
 'use strict'
 
+function mixins (context, ...args) {
+  const mixins = []
+  context.mixins(mixin => {
+    mixins.push(mixin)
+  })
+
+  const className = context.constructor.name
+  const tempClass = { [className]: class extends context.constructor {} }
+  mixins.forEach(Mixin => {
+    const methodNames = Object.getOwnPropertyNames(Mixin.prototype)
+      .filter(name => name !== 'constructor')
+
+    for (let name of methodNames) {
+      const descriptor = Reflect.getOwnPropertyDescriptor(Mixin.prototype, name)
+      Reflect.defineProperty(tempClass[className].prototype, name, descriptor)
+    }
+    Object.setPrototypeOf(context, tempClass[className].prototype)
+    context.constructor = tempClass[className]
+  })
+}
+
 module.exports = class Base {
   constructor (...args) {
-    const mixins = []
-    this.mixins(mixin => {
-      mixins.push(mixin)
-    })
-
-    mixins.forEach(Mixin => {
-      const methodNames = Object.getOwnPropertyNames(Mixin.prototype)
-        .filter(name => name !== 'constructor')
-
-      class Mixed extends this.constructor {}
-      for (let name of methodNames) {
-        const descriptor = Reflect.getOwnPropertyDescriptor(Mixin.prototype, name)
-        Reflect.defineProperty(Mixed.prototype, name, descriptor)
-      }
-      Object.setPrototypeOf(this, Mixed.prototype)
-      this.constructor = Mixed
-    })
+    mixins(this, ...args)
   }
 
   mixins () {
